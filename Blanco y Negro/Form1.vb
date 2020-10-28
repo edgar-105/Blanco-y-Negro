d9abbea69bd5127d4c85a6ec1e694a37cbd82d96
edgar105@outlook.com <edgar105@outlook.com>
2020-10-28 13:46:02 -0600

    test1

---
+++ Blanco y Negro/Form1.vb
@@ -0,0 +1,92 @@
+﻿Imports System.Drawing.Imaging
+Imports System.Drawing.Drawing2D
+
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim dlg As OpenFileDialog = New OpenFileDialog()
+        dlg.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.PNG)|*.bmp;*.jpg;*.gif;*.png"
+        If dlg.ShowDialog() = DialogResult.OK Then
+            Dim img As Image = Image.FromFile(dlg.FileName)
+            Dim bm As Bitmap = New Bitmap(img.Width, img.Height)
+            Dim g As Graphics = Graphics.FromImage(bm)
+            'Dim cm As ColorMatrix = New ColorMatrix(New Single()() _
+            '     {New Single() {0.5, 0.5, 0.5, 0, 0}, _
+            '    New Single() {0.5, 0.5, 0.5, 0, 0}, _
+            '    New Single() {0.5, 0.5, 0.5, 0, 0}, _
+            '    New Single() {0, 0, 0, 1, 0}, _
+            '    New Single() {0, 0, 0, 0, 1}})
+
+
+            'Gilles Khouzams colour corrected grayscale shear
+
+            Dim cm As ColorMatrix = New ColorMatrix(New Single()() _
+                 {New Single() {0.3, 0.3, 0.3, 0, 0}, _
+                New Single() {0.59, 0.59, 0.59, 0, 0}, _
+                New Single() {0.11, 0.11, 0.11, 0, 0}, _
+                New Single() {0, 0, 0, 1, 0}, _
+                New Single() {0, 0, 0, 0, 1}})
+
+
+            Dim attributes As ImageAttributes = New ImageAttributes()
+            attributes.SetColorMatrix(cm)
+            g.DrawImage(img, New Rectangle(0, 0, img.Width, img.Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, attributes)
+            g.Dispose()
+            Me.BackgroundImage = bm
+        End If
+    End Sub
+
+    Private Sub Form1_Paint(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles MyBase.Paint
+
+        'Create a bitmap in a fixed ratio to the original drawing area.
+        Dim bm As New Bitmap(CInt(Me.ClientSize.Width / 5), CInt(Me.ClientSize.Height / 5))
+
+        'Create a GraphicsPath object. 
+        Dim pth As New GraphicsPath()
+
+        'Add the string in the chosen style. 
+        pth.AddString("Edgar Lopez", New FontFamily("Verdana"), CInt(FontStyle.Regular), 100, New Point(20, 20), StringFormat.GenericTypographic)
+
+        'Get the graphics object for the image. 
+        Dim g As Graphics = Graphics.FromImage(bm)
+
+        'Create a matrix that shrinks the drawing output by the fixed ratio. 
+        Dim mx As New Matrix(1.0F / 5, 0, 0, 1.0F / 5, -(1.0F / 5), -(1.0F / 5))
+
+        'Choose an appropriate smoothing mode for the halo. 
+        g.SmoothingMode = SmoothingMode.AntiAlias
+
+        'Transform the graphics object so that the same half may be used for both halo and text output. 
+        g.Transform = mx
+
+        'Using a suitable pen...
+        Dim p As New Pen(Color.Red, 3)
+
+        'Draw around the outline of the path
+        g.DrawPath(p, pth)
+
+        'and then fill in for good measure. 
+        g.FillPath(Brushes.Yellow, pth)
+
+        'We no longer need this graphics object
+        g.Dispose()
+
+        'this just shifts the effect a little bit so that the edge isn't cut off in the demonstration
+        e.Graphics.Transform = New Matrix(1, 0, 0, 1, 50, 50)
+
+        'setup the smoothing mode for path drawing
+        e.Graphics.SmoothingMode = SmoothingMode.AntiAlias
+
+        'and the interpolation mode for the expansion of the halo bitmap
+        e.Graphics.InterpolationMode = InterpolationMode.HighQualityBicubic
+
+        'expand the halo making the edges nice and fuzzy. 
+        e.Graphics.DrawImage(bm, ClientRectangle, 0, 0, bm.Width, bm.Height, GraphicsUnit.Pixel)
+
+        'Redraw the original text
+        e.Graphics.FillPath(Brushes.Beige, pth)
+
+        'and you're done. 
+        pth.Dispose()
+    End Sub 'Form1_Paint
+End Class
---
+++ Blanco y Negro/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Blanco y Negro")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("Blanco y Negro")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2011")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("2697613e-f0c4-40e4-9119-82e5cdcbf121")> 
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+' mediante el asterisco ('*'), como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
